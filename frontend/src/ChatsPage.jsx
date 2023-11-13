13705696421ad77c686107dd4680c1e84ddce923
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-13 18:21:41 -0500

    fixed bugs in code, installed new chat dependecy

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,13 +1,17 @@
-import {MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advance'
-
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+//import { ChatEngine } from 'react-chat-engine-advance'
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic('353502a4-79a4-4aa1-adc5-a533204d58c2', 
-    props.user.username, props.user.secret)
-    return ( <div style={{height: '100vh'}}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
-    </div>
+    
+    return ( 
+    <div style={{height: '100vh'}}>
+        <PrettyChatWindow
+        projectId='353502a4-79a4-4aa1-adc5-a533204d58c2'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{height: '100%' }}
+        />
+     </div>   
     )
 }
 
